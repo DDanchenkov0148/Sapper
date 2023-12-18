f91937c94d06622c9c5ec07253748413565eb2d5
DDanchenkov0148 <danchenkov0148@gmail.com>
2023-12-18 13:52:07 +0200

    Добавление метода расположения мин, прохода по ячейкам игрового поля и проверки окружных клеток

--- Sapper.java
+++ Sapper.java
@@ -18,7 +18,6 @@ public class Sapper extends JFrame {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 10; j++) {
                 buttons[i][j] = new JButton();
-
                 buttons[i][j].addActionListener(new CellClickListener(i, j));
                 add(buttons[i][j]);
             }
@@ -59,4 +58,60 @@ public class Sapper extends JFrame {
         // Show all the mines on the board by uncovering them
     }
 }
+private void placeMines() {
+    Random random = new Random();
 
+    int placedMines = 0;
+
+    while (placedMines < 10) {
+        int i = random.nextInt(9);
+        int j = random.nextInt(9);
+        if (!mine[i][j]) {
+            mine[i][j] = true;
+            placedMines++;
+        }
+    }
+}
+
+private void countSurroundingMines() {
+    // Проход по всем ячейкам игрового поля
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9; j++) {
+            // Если текущая ячейка не содержит мины
+            if (!mine[i][j]) {
+                    int count = 0;
+                    // Проверка окружных ячеек и увеличить количество, если есть
+                    // Верхние клетки
+                if (i > 0 && mine[i - 1][j]) count++;
+                    // Нижние клетки
+                if (i < 8 && mine[i + 1][j]) count++;
+                    // Клетки слева
+                if (j > 0 && mine[i][j - 1]) count++;
+                    // Клетки справа
+                if (j < 8 && mine[i][j + 1]) count++;
+                    // Верхняя левая диагональ
+                if (i > 0 && j > 0 && mine[i - 1][j - 1]) count++;
+                    // Нижняя правая диагональ
+                if (i < 8 && j < 8 && mine[i + 1][j + 1]) count++;
+                    // Верхняя правая диагональ
+                if (i > 0 && j < 8 && mine[i - 1][j + 1]) count++;
+                    // Нижняя левая диагональ
+                if (i < 8 && j > 0 && mine[i + 1][j - 1]) count++;
+                    // создание массива количества мин
+                countMine[i][j] = count;
+            }
+        }
+    }
+}
+
+// метод выявления окружных клеток
+private void uncoverCircleCells(int i, int j) {
+    if (i > 0 && buttons[i - 1][j].isEnabled()) uncoverCell(i - 1, j);
+    if (i < 9 && buttons[i + 1][j].isEnabled()) uncoverCell(i + 1, j);
+    if (j > 0 && buttons[i][j - 1].isEnabled()) uncoverCell(i, j - 1);
+    if (j < 9 && buttons[i][j + 1].isEnabled()) uncoverCell(i, j + 1);
+    if (i > 0 && j > 0 && buttons[i - 1][j - 1].isEnabled()) uncoverCell(i - 1, j - 1);
+    if (i < 9 && j < 9 && buttons[i + 1][j + 1].isEnabled()) uncoverCell(i + 1, j + 1);
+    if (i > 0 && j < 9 && buttons[i - 1][j + 1].isEnabled()) uncoverCell(i - 1, j + 1);
+    if (i < 9 && j > 0 && buttons[i + 1][j - 1].isEnabled()) uncoverCell(i + 1, j - 1);
+}
