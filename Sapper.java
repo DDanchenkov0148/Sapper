60484e09472891a33cb72c3e0ce9e0f83c879c27
DDanchenkov0148 <danchenkov0148@gmail.com>
2023-12-18 13:18:38 +0200

    Update Sapper.java

--- Sapper.java
+++ Sapper.java
@@ -1,2 +1,62 @@
-public class Sapper {
+import javax.swing.*;
+import java.awt.*;
+
+public class Sapper extends JFrame {
+    private JButton[][] buttons;
+    private int[][] countMine;
+    private boolean[][] mine;                // двумерные массивы кнопок, количества мин и статуса мин
+    private int countCells;                  // количество клеток
+
+    public Sapper() {
+        setTitle("Sapper");                         // надпись в название окна
+        setDefaultCloseOperation(EXIT_ON_CLOSE);    // закрытие программы по завершению
+        setLayout(new GridLayout(9, 9));            // размер игрового поля
+        buttons = new JButton[9][9];
+        countMine = new int[10][10];
+        countCells = 0;
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 10; j++) {
+                buttons[i][j] = new JButton();
+
+                buttons[i][j].addActionListener(new CellClickListener(i, j));
+                add(buttons[i][j]);
+            }
+        }
+        placeMine();                  // расположение мин
+        countcircle();                // количество мин вокруг кнопки
+        pack();                       // адаптация размеров клетки под окно
+        setVisible(true);
+    }
+
+    private void placeMines() {
+    }
+
+    private void uncoverCell(int i, int j) {
+        // количество открытых клеток
+    }
+
+    private void uncoverCircleCells(int i, int j) {
+        // количество клеток вокруг
+    }
+
+    private void countCircleMines() {
+        // количество мин вокруг
+    }
+
+    private class CellClickListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+        }
+    }
+
+    private void winGame() {
+        // Алгоритм действий программы после победы в игре
+    }
+
+    private void loseGame() {
+        // Show a message dialog displaying a loss message
+        // Disable all the buttons to prevent further actions
+        // Show all the mines on the board by uncovering them
+    }
 }
+
